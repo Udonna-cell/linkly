be70e1bbc51006f12a2d4f54b8d57c9a5566a1ff
udonna-cell <orabuezestanley@gmail.com>
2024-12-21 14:16:48 +0100

    done with desktop

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const sass = require("sass")
+const path = require("path")
+const fs = require("fs")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let css = sass.compile(path.resolve(__dirname, "../sass/style.scss")).css
+  try {
+    fs.writeFileSync(path.resolve(__dirname, "../public/stylesheets/style.css"),css)
+  } catch (error) {
+    console.error("faild to style the page")
+  }
   res.render('index', { title: 'Express' });
 });
 
