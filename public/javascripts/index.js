92cac00a36ac06c5fc0a8f62785a111c7b28d776
udonna-cell <orabuezestanley@gmail.com>
2024-12-23 10:28:04 +0100

    added an animation on scroll for and aside section for theme selection

---
+++ public/javascripts/index.js
@@ -0,0 +1,20 @@
+// Select all sections
+const aside = document.querySelector("aside");
+
+// Function to check if the user has scrolled 30% of the viewport height
+function checkScroll() {
+  const scrollPosition = window.scrollY; // The current scroll position
+
+  const screenHeight = window.innerHeight; // The height of the viewport
+  const triggerPoint = screenHeight * 0.1; // 30% of the viewport height
+
+  if (scrollPosition >= triggerPoint) {
+    // Add the visible class to the section
+    aside.classList.add("visible");
+  } else {
+    aside.classList.remove("visible");
+  }
+}
+
+// Listen for the scroll event
+window.addEventListener("scroll", checkScroll);
