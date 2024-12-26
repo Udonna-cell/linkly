3ce1196e74604cb8bbc90e17b2a4e9798cccbd5b
Stanley-Orabueze <67174967+Udonna-cell@users.noreply.github.com>
2024-12-26 01:45:44 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -49,11 +49,11 @@ router.get("/", async function (req, res, next) {
         console.error("Failed to compile SASS:", error);
         return res.status(500).send("Failed to compile CSS");
       }
-      let ws = process.env.WS || "ws://192.168.40.92:3000/"
+      let wss = process.env.WS || "ws://192.168.40.92:3000/"
       results = (JSON.parse(JSON.stringify(results)));
       console.log(results);
       // Render the page with results
-      res.render("index", { title: "Express", isEmpty, results, user, ws });
+      res.render("index", { title: "Express", isEmpty, results, user, wss });
     }
   );
 });
