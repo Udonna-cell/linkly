8ae319e02cf8c7b5981ce986859eb0e1f46407a9
udonna-cell <orabuezestanley@gmail.com>
2024-12-29 02:38:50 +0100

    fixed client action on copy error

--- utility/actionAdder.js
+++ utility/actionAdder.js
@@ -1,6 +1,7 @@
 function actionAdder(data){
     data = data.map((obj)=>{
         obj.action = `download(${obj.id})`
+        obj.copy = `copyToClipboard(${obj.short})`
         return obj
     })
     return data
